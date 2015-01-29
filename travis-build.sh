b45030c18bf903228c9e92ffcb96236f6d1da553
Graeme Rocher <graemerocher@Graemes-iMac.local>
2015-01-29 08:19:31 +0100

    Release 3.0.0.M1

--- travis-build.sh
+++ travis-build.sh
@@ -8,7 +8,7 @@ echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
 
 export GRADLE_OPTS="-Xmx2048m -Xms256m -XX:MaxPermSize=512m -XX:+CMSClassUnloadingEnabled -XX:+HeapDumpOnOutOfMemoryError"
 
-./gradlew assemble 
+./gradlew assemble
 
 if [[ $TRAVIS_PULL_REQUEST == 'false' ]]; then
 
@@ -26,18 +26,12 @@ if [[ $TRAVIS_PULL_REQUEST == 'false' ]]; then
 	if [[ -n $TRAVIS_TAG ]]; then
 		version="$TRAVIS_TAG"
 		version=${version:1}
-        zipName="grails-docs-$version"
-        export RELEASE_FILE="${zipName}.zip"			
+    zipName="grails-docs-$version"
+    export RELEASE_FILE="${zipName}.zip"
 
-		github-release upload \
-		    --user grails \
-		    --repo grails-core \
-		    --tag $TRAVIS_TAG \
-		    --name "grails-docs-${version}.zip" \
-		    --file "build/distributions/$RELEASE_FILE"
+#		github-release upload --user grails --repo grails-core --tag $TRAVIS_TAG --name grails-docs-${version}.zip --file build/distributions/$RELEASE_FILE
 
-
-		milestone=${version:5}		
+		milestone=${version:5}
 		if [[ -n $milestone ]]; then
 			git rm -rf latest/
 			mkdir -p latest
@@ -50,16 +44,16 @@ if [[ $TRAVIS_PULL_REQUEST == 'false' ]]; then
 
 		mkdir -p "$version"
 		cp -r ../build/docs/. "./$version/"
-		git add "$version/*"			
+		git add "$version/*"
 
 		mkdir -p "$majorVersion"
 		cp -r ../build/docs/. "./$majorVersion/"
-		git add "$majorVersion/*"						
+		git add "$majorVersion/*"
 
-	fi		
+	fi
 
 	git commit -a -m "Updating docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
 	git push origin HEAD
 	cd ..
-	rm -rf gh-pages	
+	rm -rf gh-pages
 fi
