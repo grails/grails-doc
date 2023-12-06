8f00a7a9ea618a074d5c6d7f088a3dae5a48355e
Puneet Behl <behlp@unityfoundation.io>
2023-12-06 13:00:50 +0530

    Update GitHub Workflow Config (#880)
    - Update email address
    - Skip Latest and Snapshot for 5.3.x branch

--- .github/actions/tag-and-release/entrypoint.sh
+++ .github/actions/tag-and-release/entrypoint.sh
@@ -29,6 +29,5 @@ echo "Pushing release version and recreating v${grails_version} tag"
 git add gradle.properties
 git commit -m "Release v${grails_version} docs"
 git push origin $TARGET_BRANCH
-git push origin :refs/tags/v${grails_version}
 git tag -fa v${grails_version} -m "Release v${grails_version} docs"
-git push origin $TARGET_BRANCH --tags
+git push -f origin v${grails_version}
